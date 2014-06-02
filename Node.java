9a10f5acfec34c20a11e794a94bf16d9b1a1c5b5
SpencerWie <spdwiecz@iupui.edu>
2014-06-02 15:21:12 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+	public Node left;
+	public Node right;
+	public int value;
+
+	public Node(Node left , Node right, int value){
+		this.left = left;
+		this.right = right;
+		this.value = value;
+	}
+}
